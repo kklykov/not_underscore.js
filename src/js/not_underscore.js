12a11fedb84c9ee53623c4eb882537b68fbb6c00
Konstantin Klykov <kklykov@gmail.com>
2020-03-04 21:56:09 +0300

    not_.map() and not_.reduce() functions added
    work at same way as underscore functions

--- src/js/not_underscore.js
+++ src/js/not_underscore.js
@@ -26,20 +26,90 @@
 
   // I took the previous code from the original underscore library
 
-  not_.each = function(list, iteratee, context){
+  /*
+    function: .each
+    parameters -> 
+                list: 
+                iteratee:
+  */
+
+  not_.each = function(list, iteratee){
     if(list instanceof Array){
         list.forEach((element, index, list)=> {
             iteratee(element, index, list)
-        });
+        })
     }else{
-        let index = 0
         for(const prop in list){
-            iteratee(list[prop], index, list)
-            index++
+            iteratee(list[prop], prop, list)
         }
     }
-    return list;
+    return list
   }
+
+   /*
+    function: .map
+    parameters -> 
+                list: 
+                iteratee:
+  */
+
+  // not_.map = function(list, iteratee){
+  //   if(list instanceof Array){
+  //       let array = []
+  //       list.forEach((element, index, list)=> {
+  //         array[index] = iteratee(element, index, list)
+  //       })
+  //       return array
+  //   }else{
+  //       let obj = {}
+  //       for(const prop in list){
+  //         obj[prop] = iteratee(list[prop], prop, list)
+  //       }
+  //       return obj
+  //   }
+  // }
+
+    not_.map = function(list, iteratee){
+      let array = []
+      if(list instanceof Array){
+          list.forEach((element, index, list)=> {
+            array[index] = iteratee(element, index, list)
+          })
+      }else{
+          let index = 0
+          for(const prop in list){
+            array[index] = iteratee(list[prop], prop, list)
+            index++
+          }
+      }
+      return array
+    }
+
+
+   /*
+    function: .map
+    parameters -> 
+                list: 
+                iteratee:
+                [memo]:
+  */
+
+    not_.reduce = function(list, iteratee, memo){
+        let count
+        not_.map(list, function(value, index, self) {
+          if(!count){
+            if(!memo){
+              memo = value
+            }else{
+              memo = iteratee(memo, value, index, self)
+            }
+          }else{
+            memo += iteratee(memo, value, index, self)
+          }
+          count++
+        })
+        return memo
+    }
 
 
 })();
